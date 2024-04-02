dd0b93535de995bd718ab5647626f24f2db49cb4
Oscar Adolfo Méndez Muñoz <cocamendez974@gmail.com>
2024-04-02 13:53:54 -0600

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.farasi.finale',
+  appName: 'Finale Educativa',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
